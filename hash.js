89c6684c854a7774c6169cb159f093e1f24b5beb
Irfan Belim <belimirfan1989@gmail.com>
2026-09-22 12:04:14 +0530

    Initial commit

---
+++ hash.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+async function main() {
+  let password = "mypass";
+
+  // Hash the password
+  let hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log("Original Password:", password);
+  console.log("Hashed Password:", hashedPassword);
+}
+
+main();
